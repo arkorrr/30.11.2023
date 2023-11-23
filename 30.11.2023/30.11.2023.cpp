1132ae6b08087a847db6cfe1629fd164a8a0050a
trakt <trakt@DESKTOP-UDGVRCB>
2023-11-23 17:49:29 +0300

    Добавьте файлы проекта.

---
+++ 30.11.2023/30.11.2023.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    string surnames[5];
+
+    for (int i = 0; i < 5; ++i) 
+    {
+        cout << "Enter surname:";
+        cin >> surnames[i];
+    }
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4 - i; ++j) {
+            if (surnames[j] > surnames[j + 1]) {
+                swap(surnames[j], surnames[j + 1]);
+            }
+        }
+    }
+
+    cout << "Entered last names, sorted alphabetically: " << endl;
+    for (int i = 0; i < 5; ++i) {
+        cout << surnames[i] << endl;
+    }
+
+    return 0;
+}
